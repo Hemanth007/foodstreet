d664bb6aeaa79b12d2c2bc178bf22fee115c20c7
Hemanth007 <dhemanthkumarg@gmail.com>
2018-03-17 13:03:06 +0530

    Committed Initial Code

---
+++ foodstreet/src/main/java/com/dhk/foodstreet/App.java
@@ -0,0 +1,13 @@
+package com.dhk.foodstreet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
